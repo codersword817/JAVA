7270bf304d3008dbde4ef6d6105bde603a6b1980
aakashmaroju <marojuaakash2003@gmail.com>
2024-09-05 09:13:16 +0530

    Introduction to Java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        int n=10;
+        int n2=20;
+        int result = n+n2;
+        System.out.println(result);
+    }
+}
